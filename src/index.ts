25f68df236ed17c96a282e8394445025d2d53049
Kushagra <skushagra.sharma@gmail.com>
2024-11-21 00:48:27 +0530

    added-cors

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import userRoutes from './routes/user';
 import mintRoutes from './routes/mint';
+import cors from 'cors';
 
 const app = express();
 app.use(express.json());
+app.use(cors())
 
 app.use('/nft/user', userRoutes);
 app.use('/nft/mint', mintRoutes);
